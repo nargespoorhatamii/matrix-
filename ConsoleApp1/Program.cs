4aaf344a87484493e29398cbd93e6c163ca220e2
ThinkPad <ThinkPad@DESKTOP-Q88TTS3>
2023-03-10 03:15:41 +0330

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using systam;
+
+namespace Project
+{
+    class test
+    {
+        public static void main;{
+        int[3, 3] matrix = new int[3, 3];
+        string input_1 = Console.ReadLine();
+        int number = Convert.ToInt32(Console.ReadLine());
+        int counter = 0;
+       for(int i=0;i<=2;i++)
+            {for(int j=0;ja<=2;j++)
+            {Console.ReadLine(int[i, j] matrix); }
+            }
+       if (int[3, 3]matrix==number)
+        { Console.WriteLine(counter); }
+        else {Console.WriteLine("number haven't found") }
+
+          
+       }
+     }
+
+ }
